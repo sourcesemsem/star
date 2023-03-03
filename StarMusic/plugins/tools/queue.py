af92b4da8422c53af1ceb913777002202bad875d
DaRrkNneSs1 <108493023+DaRrkNneSs1@users.noreply.github.com>
2023-03-04 00:01:01 +0200

    Add files via upload

--- StarMusic/plugins/tools/queue.py
+++ StarMusic/plugins/tools/queue.py
@@ -20,7 +20,7 @@ from config import BANNED_USERS
 from strings import get_command
 from StarMusic import app
 from StarMusic.misc import db
-from StarMusic.utils import (StarMusicbin, get_channeplayCB,
+from StarMusic.utils import (Starbin, get_channeplayCB,
                               seconds_to_min)
 from StarMusic.utils.database import (get_cmode, is_active_chat,
                                        is_music_playing)
@@ -216,7 +216,7 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
             )
         if "🏷" in msg:
             msg = msg.replace("🏷", "")
-        link = await StarMusicbin(msg)
+        link = await Starbin(msg)
         med = InputMediaPhoto(
             media=link, caption=_["queue_3"].format(link)
         )
