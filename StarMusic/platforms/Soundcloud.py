41210d1331df1773a7afb05528a469fa0d097aff
DaRrkNneSs1 <108493023+DaRrkNneSs1@users.noreply.github.com>
2023-03-03 23:17:23 +0200

    Add files via upload

--- StarMusic/platforms/Soundcloud.py
+++ StarMusic/platforms/Soundcloud.py
@@ -12,7 +12,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from STARMUSIC.utils.formatters import seconds_to_min
+from StarMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
--- StarMusic/platforms/Telegram.py
+++ StarMusic/platforms/Telegram.py
@@ -18,7 +18,7 @@ from pyrogram.types import (InlineKeyboardButton,
 
 import config
 from config import MUSIC_BOT_NAME, lyrical
-from STARMUSIC import app
+from StarMusic import app
 
 from ..utils.formatters import (convert_bytes, get_readable_time,
                                 seconds_to_min)
