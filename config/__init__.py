6c4f5235f37b67546746a098969aa5b62af0993f
DaRrkNneSs1 <108493023+DaRrkNneSs1@users.noreply.github.com>
2023-03-03 20:50:00 +0200

    Add files via upload

--- config/__init__.py
+++ config/__init__.py
@@ -1,11 +1,10 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by TeamStar@Github, < https://github.com/DaRrKNneSs_1 >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/DaRrkNneSs1/STAR_MUSIC > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/DaRrkNneSs1/STAR_MUSIC/blob/master/LICENSE >
 #
 # All rights reserved.
-#
 
 from .config import *
