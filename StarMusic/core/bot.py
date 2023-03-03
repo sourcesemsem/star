ef2eaf5031fe4415993949e758c19781b9e6599a
DaRrkNneSs1 <108493023+DaRrkNneSs1@users.noreply.github.com>
2023-03-03 22:49:46 +0200

    Add files via upload

--- StarMusic/core/bot.py
+++ StarMusic/core/bot.py
@@ -17,11 +17,11 @@ import config
 from ..logging import LOGGER
 
 
-class YukkiBot(Client):
+class StarBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "YukkiMusicBot",
+            "StarMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
