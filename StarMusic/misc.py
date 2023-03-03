d16aca9c5da26c25535e47c7a1d5b542fa846b38
DaRrkNneSs1 <108493023+DaRrkNneSs1@users.noreply.github.com>
2023-03-03 22:58:13 +0200

    Add files via upload

--- StarMusic/misc.py
+++ StarMusic/misc.py
@@ -14,7 +14,7 @@ import heroku3
 from pyrogram import filters
 
 import config
-from STARMUSIC.core.mongo import pymongodb
+from StarMusic.core.mongo import pymongodb
 
 from .logging import LOGGER
 
