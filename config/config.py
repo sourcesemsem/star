2a96d05751be452238406383ab7af97b9f4d8f6e
DaRrkNneSs1 <108493023+DaRrkNneSs1@users.noreply.github.com>
2023-03-04 02:49:17 +0200

    Add files via upload

--- config/config.py
+++ config/config.py
@@ -65,10 +65,10 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/S0URCE_STAR")
+    "SUPPORT_CHANNEL", None
 )  # Example:- https://t.me/S0URCE_STAR
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/SUPP0RT_STAR")
+    "SUPPORT_GROUP", None
 )  # Example:- https://t.me/SUPP0RT_STAR
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
